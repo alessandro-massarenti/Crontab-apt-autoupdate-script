9febc090797ae064a127be2ff35146115f431a93
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2019-05-17 14:50:05 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-apt-get update &&
-apt-get -y upgrade &&
-apt-get -y autoremove
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo"*****************************";
+echo "Lavoro iniziato alle:\n$dt";
+echo"*****************************";
+apt-get update;
+apt-get -y upgrade;
+apt-get -y autoremove;
