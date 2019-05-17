db9e842532d0d640639f1661c60912dae97f3ad4
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2019-05-17 15:42:39 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-echo " ";
-dt = $(date '+%d/%m/%Y %H:%M:%S');
-echo "*****************************";
-echo "Lavoro iniziato alle:";
-echo "$dt";
-echo "*****************************";
-apt-get update;
-apt-get -y upgrade;
-apt-get -y autoremove;
-dt = $(date '+%d/%m/%Y %H:%M:%S');
-echo "*****************************";
-echo "Lavoro completato alle:";
-echo "$dt";
-echo "*****************************";
-echo " ";
+echo 
+dt=$(date '+%d/%m/%Y %H:%M:%S')
+echo -e "*****************************"
+echo -e "Lavoro iniziato alle:"
+echo -e "$dt"
+echo -e "*****************************"
+apt-get update
+apt-get -y upgrade
+apt-get -y autoremove
+dt=$(date '+%d/%m/%Y %H:%M:%S')
+echo "*****************************"
+echo "Lavoro completato alle:"
+echo "$dt"
+echo "*****************************"
+echo
